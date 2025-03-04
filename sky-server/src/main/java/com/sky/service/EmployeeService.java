0a9a4c269047d90a930c6cec92baf61d0eb3791a
1dAnderson <2482778542@qq.com>
2025-03-04 16:43:40 +0800

    新增员工业务代码开发

--- sky-server/src/main/java/com/sky/service/EmployeeService.java
+++ sky-server/src/main/java/com/sky/service/EmployeeService.java
@@ -9,9 +9,13 @@ public interface EmployeeService {
     /**
      * 员工登录
      * @param employeeLoginDTO
-     * @return
+     * 
      */
     Employee login(EmployeeLoginDTO employeeLoginDTO);
 
+    /**
+     * 新增员工
+     * @param employeeDTO
+     */
     void save(EmployeeDTO employeeDTO);
 }
